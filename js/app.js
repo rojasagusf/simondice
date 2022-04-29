14da1546d9016ec45a6d92c2f4dda06d56c9f794
Agustin Rojas <rojasagusf01@gmail.com>
2022-04-29 15:25:26 -0300

    agregado

--- js/app.js
+++ js/app.js
@@ -2,26 +2,54 @@
 
 const $start = document.querySelector("#start");
 const $stop = document.querySelector("#stop");
-const $tryAgain = document.querySelector("#try");
-const $title = document.querySelector("#press");
 
 
+
+
+
+
+/*
+1- Al hacer click en el botón START: 
+   a) -Se cambia a boton STOP. --------- FINISHED
+   b) -Se actualiza el tablero a ROUND y TURN.
+   c) -Inicia el proceso del juego.
+
+  b) se debe ocultar el h2 y desocultar los siguientes h2.
+    se debe escribir el round: {numero de ronda} y turn: {player}. 
+*/
+
+escrituraDinamica("Press start");
+
+$start.onclick = () => {
+  alternarBotones();
+  actualizarTablero();
+}
+
+$stop.onclick = () => {
+  location.reload();
+}
+
+
+function alternarBotones() {
+  $start.classList = "oculto";
+  $stop.className = "stop";
+}
 
 function escrituraDinamica(texto) {
-  
+  const $title = document.querySelector("#press");
   let time = 100;
   
   for(let i = 0; i < texto.length; i++) {
-    console.log(texto[i]);
     let caracter = texto[i];
+
     setTimeout(() => {
       if($title.innerText === "Press") {
         $title.innerText += ` ${texto[i]}`;
       } else {
         $title.innerText += caracter;
       }
-        
     }, time);
+
     time += 70;
   }
  
@@ -30,10 +58,27 @@ function escrituraDinamica(texto) {
   },time);
 }
 
-escrituraDinamica("Press start");
+function actualizarTablero() {
+  const $title = document.querySelector("#press");
+  const $turn = document.querySelector("#turn");
+  const $round = document.querySelector("#round");
 
-/*
+  
+
+  $title.className = "oculto";
+  $turn.className = "";
+  $round.className = "";
+
+  escrituraDinamica($turn.innerText = "Turn");
+
+
+}
+
+function inicioJuego() {
+  const patron = [];
 
 
 
-*/
+
+  
+}
